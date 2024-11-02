18b98ca08400181002703ec703d3255624904402
cahyaningerdinira <123143610+erdiniraaa@users.noreply.github.com>
2024-11-02 14:58:28 +0800

    Add files via upload

---
+++ WebTokoBuku/admin/assets/js/custom.js
@@ -0,0 +1,199 @@
+
+
+(function ($) {
+    "use strict";
+    // mendeklarasi menu utama dimana fungsi diatur menggunakan halaman web
+    var mainApp = {
+
+        main_fun: function () {
+            //metis menu untuk sidebarnya (untuk membuat menu navigasi samping)
+            $('#main-menu').metisMenu();
+
+            // mengatur sidebar berdasarkan ukuran layar tampilan web
+            $(window).bind("load resize", function () {
+                if ($(this).width() < 768) {
+                    $('div.sidebar-collapse').addClass('collapse')
+                } else {
+                    $('div.sidebar-collapse').removeClass('collapse')
+                }
+            });
+
+            // grafik moris chart nah ini kesambung sama Moris.js
+            Morris.Bar({
+                element: 'morris-bar-chart',
+                data: [{
+                    y: '2006',
+                    a: 100,
+                    b: 90
+                }, {
+                    y: '2007',
+                    a: 75,
+                    b: 65
+                }, {
+                    y: '2008',
+                    a: 50,
+                    b: 40
+                }, {
+                    y: '2009',
+                    a: 75,
+                    b: 65
+                }, {
+                    y: '2010',
+                    a: 50,
+                    b: 40
+                }, {
+                    y: '2011',
+                    a: 75,
+                    b: 65
+                }, {
+                    y: '2012',
+                    a: 100,
+                    b: 90
+                }],
+                xkey: 'y',
+                ykeys: ['a', 'b'],
+                labels: ['Series A', 'Series B'],
+                hideHover: 'auto',
+                resize: true
+            });
+
+            //grafik donut chart jadi ini untuk menunjukan proposi dari 3 kategori di bawah
+            Morris.Donut({
+                element: 'morris-donut-chart',
+                data: [{
+                    label: "Download Sales",
+                    value: 12
+                }, {
+                    label: "In-Store Sales",
+                    value: 30
+                }, {
+                    label: "Mail-Order Sales",
+                    value: 20
+                }],
+                resize: true
+            });
+
+            
+            //grafik moris area chart ini untuk menggambarkan penjualan kami 
+            Morris.Area({
+                element: 'morris-area-chart',
+                data: [{
+                    period: '2010 Q1',
+                    iphone: 2666,
+                    ipad: null,
+                    itouch: 2647
+                }, {
+                    period: '2010 Q2',
+                    iphone: 2778,
+                    ipad: 2294,
+                    itouch: 2441
+                }, {
+                    period: '2010 Q3',
+                    iphone: 4912,
+                    ipad: 1969,
+                    itouch: 2501
+                }, {
+                    period: '2010 Q4',
+                    iphone: 3767,
+                    ipad: 3597,
+                    itouch: 5689
+                }, {
+                    period: '2011 Q1',
+                    iphone: 6810,
+                    ipad: 1914,
+                    itouch: 2293
+                }, {
+                    period: '2011 Q2',
+                    iphone: 5670,
+                    ipad: 4293,
+                    itouch: 1881
+                }, {
+                    period: '2011 Q3',
+                    iphone: 4820,
+                    ipad: 3795,
+                    itouch: 1588
+                }, {
+                    period: '2011 Q4',
+                    iphone: 15073,
+                    ipad: 5967,
+                    itouch: 5175
+                }, {
+                    period: '2012 Q1',
+                    iphone: 10687,
+                    ipad: 4460,
+                    itouch: 2028
+                }, {
+                    period: '2012 Q2',
+                    iphone: 8432,
+                    ipad: 5713,
+                    itouch: 1791
+                }],
+                xkey: 'period',
+                ykeys: ['iphone', 'ipad', 'itouch'],
+                labels: ['iPhone', 'iPad', 'iPod Touch'],
+                pointSize: 2,
+                hideHover: 'auto',
+                resize: true
+            });
+
+           //moris line chart melihat tren dari waktu" melihat data yang di ubah seiring waktu
+            Morris.Line({
+                element: 'morris-line-chart',
+                data: [{
+                    y: '2006',
+                    a: 100,
+                    b: 90
+                }, {
+                    y: '2007',
+                    a: 75,
+                    b: 65
+                }, {
+                    y: '2008',
+                    a: 50,
+                    b: 40
+                }, {
+                    y: '2009',
+                    a: 75,
+                    b: 65
+                }, {
+                    y: '2010',
+                    a: 50,
+                    b: 40
+                }, {
+                    y: '2011',
+                    a: 75,
+                    b: 65
+                }, {
+                    y: '2012',
+                    a: 100,
+                    b: 90
+                }],
+                xkey: 'y',
+                ykeys: ['a', 'b'],
+                labels: ['Series A', 'Series B'],
+                hideHover: 'auto',
+                resize: true
+            });
+           
+     
+        },
+
+            //ini untuk menginilisasi semua fitur fitur
+        initialization: function () {
+            mainApp.main_fun();
+
+        }
+
+    }
+    // Initializing ini ketika halaman web dijalankan yang memulai semua fungsi di atas 
+
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+
+}(jQuery));
+
+// kenapa menggunakan sidebar agar dapat menyesuaikan lebar layar, dan grafik 
+
+//kenapa harus mamakai moris.js untuk menampilkan berbagai grafik di atas yang telah di buat = kenapa harus pake grafik ?
+// digunakan untuk menampulkan tren dari buku dan distribusi atau perbandingan 
---
+++ WebTokoBuku/admin/detail.php
@@ -0,0 +1,93 @@
+<h2>Detail Pembelian</h2>
+
+// Mengembalian data pembelian 
+<?php  
+	$ambil=$koneksi->query("SELECT * FROM pembelian JOIN pelanggan ON pembelian.id_pelanggan=pelanggan.id_pelanggan WHERE pembelian.id_pembelian='$_GET[id]'");
+	$detail=$ambil->fetch_assoc();
+
+	// echo "<pre>";
+	// print_r($detail);
+	// echo "</pre>";
+	
+?>
+
+// Menampilkan informasi pembelian 
+				Kode Pembelian : <strong>H-<?php echo $detail['id_pembelian']; ?>-S</strong><br>
+				Tanggal Pembelian : <?php echo $detail['tanggal_pembelian']; ?> <br>
+				Harga Pembelian : Rp. <?php echo number_format($detail['total_pembelian'])?><br>
+				Status Pembelian : <strong><?php echo $detail['status_pembelian']; ?></strong><br>
+				<?php if (!empty($detail['resi_pengiriman'])): ?>
+							No.Resi : <strong><?php echo $detail['resi_pengiriman']; ?></strong>
+							<?php endif  ?>
+// Informasi pelanggan, pengirim, dan alamat pengirim 
+			<div class="row">
+				<div class="col-md-4">
+
+					<h3>Pelanggan</h3>
+					<strong><?php echo $detail['nama_pelanggan']?></strong><br>
+						Nomor Telepon :  <?php echo $detail['telepon_pelanggan']?><br>Gmail : <?php echo $detail['gmail_pelanggan']; ?>
+		
+				</div>
+				<div class="col-md-4">
+					<h3>Pengirim</h3>
+					<strong><?php echo $detail['nama_kurir']; ?></strong><br>
+				Tarif : Rp. <?php echo number_format($detail['tarif']); ?>
+				</div>
+				<div class="col-md-4">
+					<h3>Alamat Pengiriman</h3>
+					<strong><?php echo $detail['alamat_pengiriman']; ?></strong>
+				</div>
+			</div>
+
+// Tabel detail produk yang dibeli
+			<table class="table table-bordered">
+				<thead>
+					<tr>
+						<th>No</th>
+						<th>Nama Produk</th>
+						<th>Harga</th>
+						<th>Jumlah</th>
+						<th>Total</th>
+					</tr>
+				</thead>
+
+
+				<tbody>
+					<?php $nomor=1; ?>
+					<?php $totalbelanja=0;?>
+					<?php $ambil=$koneksi->query("SELECT * FROM pembelian_produk JOIN produk ON pembelian_produk.id_produk=produk.id_produk WHERE pembelian_produk.id_pembelian='$_GET[id]'");?>
+					<?php while($pecah=$ambil->fetch_assoc()) { ?>
+						<?php $subharga =  $pecah['harga_produk']*$pecah['jumlah_pembelian']; ?>
+					<tr>
+						<td> <?php echo $nomor; ?></td>
+						<td> <?php echo $pecah['nama_produk']; ?></td>
+						<td> Rp. <?php echo number_format($pecah['harga_produk']); ?></td>
+						<td> <?php echo $pecah['jumlah_pembelian']; ?></td>
+						<td> Rp. <?php echo number_format($subharga); ?></td>
+					</tr>
+					<?php $nomor++ ?>
+					<?php $totalbelanja+=$subharga; ?>
+					<?php } ?>
+				</tbody>
+
+// menghitung rital belanja dan tampilan tarif
+				<tfoot>
+					<tr>
+						<th colspan="4">Tarif</th>
+						<td>Rp. <?php echo number_format($detail['tarif']); ?></td>
+					</tr>
+					<tr>
+						<th colspan="4">TOTAL</th>
+						<th>Rp. <?php echo number_format($totalbelanja+$detail['tarif']); ?></th>
+					</tr>
+				</tfoot>
+
+</table>
+
+// ini adalah halaman detail pembelian dalam sistem dimana ini menampilkan rincian transaksi pembelian yang di lakukan oleh pelanggan
+//Kode ini digunakan untuk:
+
+//Mengambil data pembelian dari database.
+//Menampilkan informasi pembelian seperti kode, tanggal, harga total, status, nomor resi (jika ada), dan data pelanggan.
+//Menampilkan detail produk yang dibeli beserta harga dan jumlah.
+//Menghitung total belanja yang termasuk tarif pengiriman.
